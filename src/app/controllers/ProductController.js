1ed837bf52f0e58e3f46b8ea19c6559cb283709b
Wellington Cid <wellington.cid@hotmail.com>
2020-07-22 19:49:50 -0300

    save, update and delete images

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,6 +1,8 @@
 const { formatPrice } = require('../../lib/utils')
 const Category = require('../models/Category')
 const Product = require('../models/Product')
+const File = require('../models/File')
+
 
 
 module.exports = {
@@ -26,10 +28,17 @@ module.exports = {
             }
         }
     
+        if (req.files.length == 0)
+            return res.send('Por favor, envie pelo menos uma imagem')
+
+
         let results = await Product.create(req.body)
         const productId = results.rows[0].id
 
-        return res.redirect(`/products/${productId}`)
+        const filesPromise = req.files.map(file => File.create({...file, product_id: productId}))
+        await Promise.all(filesPromise)
+
+        return res.redirect(`/products/${productId}/edit`)
 
     },
     async edit(req, res) {
@@ -41,11 +50,63 @@ module.exports = {
         product.old_price = formatPrice(product.old_price)
         product.price = formatPrice(product.price)
 
-
+        // get categories
         results = await Category.all()
         const categories = results.rows
 
-        return res.render("products/edit.njk", { product, categories })
+        // get images
+        results = await Product.files(product.id)
+        let files = results.rows
+        files = files.map(file => ({
+            ...file,
+            src: `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
+        }))
 
+        return res.render("products/edit.njk", { product, categories, files })
+
+    },
+    async put(req, res) {
+        const keys = Object.keys(req.body)
+
+        for (key of keys) {
+            if (req.body[key] == "" && key != "removed_files") {
+                return res.send('Please complete all fields.')
+            }
+        }
+
+        if (req.files.length != 0) {
+            const newFilesPromise = req.files.map(file =>
+                File.create({...file, product_id: req.body.id}))
+
+            await Promise.all(newFilesPromise)
+        }
+ 
+        if (req.body.removed_files) {
+            const removedFiles = req.body.removed_files.split(",")
+            const lastIndex = removedFiles.length - 1
+            removedFiles.splice(lastIndex, 1)
+
+            const removedFilesPromise = removedFiles.map(id => File.delete(id))
+
+            await Promise.all(removedFilesPromise)
+        }
+
+
+        req.body.price = req.body.price.replace(/\D/g, "")
+
+        if (req.body.old_price != req.body.price) {
+            const oldProduct = await Product.find(req.body.id)
+            req.body.old_price = oldProduct.rows[0].price
+        }
+
+        await Product.update(req.body)
+
+        return res.redirect(`/products/${req.body.id}/edit`)
+
+    },
+    async delete (req,res) {
+        await Product.delete(req.body.id)
+
+        return res.redirect('/products/create')
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const multer = require('./app/middlewares/multer')
 const ProductController = require('./app/controllers/ProductController')
 
 routes.get('/', function (req, res) {
@@ -9,7 +10,10 @@ routes.get('/', function (req, res) {
 
 routes.get('/products/create', ProductController.create)
 routes.get('/products/:id/edit', ProductController.edit)
-routes.post('/products', ProductController.post)
+routes.post('/products', multer.array("photos", 6), ProductController.post)
+routes.put('/products', multer.array("photos", 6), ProductController.put)
+routes.delete('/products', ProductController.delete)
+
 
 // alias
 routes.get('/ads/create', function (req, res) {
